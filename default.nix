0fd1fc72226ca20d2cb249aecc4fa4d53a88d431
Michael Brantley <36448130+limeytexan@users.noreply.github.com>
2022-04-16 03:25:04 +0100

    use patched version of nix
    ... with `nix profile import` command.

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@ let
     bashInteractive # required for read() `-i` flag
     coreutils
     dasel
+    fetchpatch
     findutils
     git
     gh
@@ -17,7 +18,20 @@ let
     unixutils
     ;
   inherit (pkgs.unixtools) getent;
-  nix = nixUnstable;
+  nix = nixUnstable.overrideAttrs (oldAttrs: {
+    patches = oldAttrs.patches ++ [
+      (fetchpatch {
+        url = "https://github.com/flox/nix/commit/07f1513e74230e3aecf9af2fb8b578b99644565f.patch";
+        sha256 = "sha256-nxzHrae0x0I2m0ub5gJObq50yzFl8vfuYzPFbEJuox0=";
+      })
+      # This doesn't work; insists upon an "installable" argument. So we'll have
+      # to build all the flakerefs independently.
+      #(fetchpatch {
+      #  url = "https://github.com/flox/nix/commit/90eea2e1085c85e1fb3d9709bd9a41d8cad4c58f.patch";
+      #  sha256 = "sha256-73XgxVlG06TH/gljlRivWC33MWCUF7yfwFoWDFkVCEo=";
+      #})
+    ];
+  });
 
 in stdenv.mkDerivation rec {
   pname = "flox";
