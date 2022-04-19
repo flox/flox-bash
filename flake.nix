386c755bbb61f54833596f3ab334ed3ce19fd067
Michael Brantley <limeytexan@users.noreply.github.com>
2022-04-19 10:23:22 +0100

    flake.nix: make package buildable/installable
    Not 100% sure I've done this right but it seems to work.
    
    The basic goal is to be able to `nix install github:flox/flox`.
    
    Also configured this to work for x86_64-darwin and aarch64-linux.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,19 @@
   outputs = {
     self,
     nixpkgs,
-  }: {
-    packages = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-darwin"] (system: {
+  }: rec {
+    packages = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system: {
       default =
         import ./default.nix
         {
           pkgs = nixpkgs.legacyPackages.${system};
         };
     });
+    defaultPackage = {
+      x86_64-linux = packages.x86_64-linux.default;
+      aarch64-linux = packages.aarch64-linux.default;
+      x86_64-darwin = packages.x86_64-darwin.default;
+      aarch64-darwin = packages.aarch64-darwin.default;
+    };
   };
 }
