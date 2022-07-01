f323b7d7fe989fdfa86386e3ed454bf393d8e450
Michael Brantley <brantley@floxdev.com>
2022-07-01 16:40:07 +0100

    Tweaking flake.nix to account for new capacitor
    Required for installer command to work as documented in the tour of flox.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   nixConfig.extra-substituters = [ "s3://flox-store-public" ];
+  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=minicapacitor";
+  inputs.capacitor.inputs.root.follows = "/";
   outputs =
     { capacitor,... } @ args: capacitor args (_: {
       packages = import ./default.nix;
