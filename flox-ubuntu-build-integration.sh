8e0ddb759e340303e45f7a8770d09c6bab2638b8
tomberek <tomberek@users.noreply.github.com>
2022-09-15 04:52:59 -0400

    ci: rename flox repo (#203)

--- flox-ubuntu-build-integration.sh
+++ flox-ubuntu-build-integration.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e 
 set -o pipefail
-export FLOXPATH="/home/ubuntu/flox"
+export FLOXPATH="/home/ubuntu/flox-internal"
 echo "**** PERFORMING BUILD OF FLOX AGAINST LATEST REVISION ****"
 export TERM="xterm-256color"
 mkdir -p "$FLOXPATH" && cd "$FLOXPATH"
 #here we used latest installed verison of flox via package to build flox
-flox build github:flox/floxpkgs#stable.flox --override-input flox github:flox/flox?rev="$1"
+flox build github:flox/floxpkgs#stable.flox --override-input flox github:flox/flox-internal?rev="$1"
 echo "**** PERFORMING FLOX DEFAULT TEMPLATE INTEGRATION TEST ****"
 #now we use the built version of flox wrapper from the PR or commit on gh to perform tests
 
-curl -O https://raw.githubusercontent.com/flox/flox/"$1"/test.bats
-bats /home/ubuntu/actions-runner/_work/flox/flox/test.bats
+curl -O https://raw.githubusercontent.com/flox/flox-internal/"$1"/test.bats
+bats test.bats
 #du -hDd0 /nix
 rm -rf myproj
 mkdir -p myproj && cd myproj
