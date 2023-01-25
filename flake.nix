6c04aef59edc6ea1a9c1e26cc9996653a33719ff
Michael Brantley <brantley@floxdev.com>
2023-01-25 10:13:06 +0000

    flakebump, plus keep floxEnv prompt in develop shell

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Floxpkgs/Project Template";
-  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
+  # Allow the floxEnv prompt to persist.
+  # nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
 
   # Declaration of external resources
