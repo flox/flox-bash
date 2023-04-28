b018128c4ab312c2637d9eb5852170e13e560c8c
Michael Brantley <36448130+limeytexan@users.noreply.github.com>
2023-04-27 22:14:34 -0400

    fix: suppress superfluous warnings (#546)
    ## Current Behavior
    
    Commit 9d0400571ba71867ce5cbba2cc7ae94a886719a2 introduced benign
    warning messages that users have complained about. The commit was
    primarily made to inform users of git-related prompts (e.g. yubikey).
    
    ## Proposed Changes
    
    An alternate way to deal with this is to prevent the prompts altogether
    by redirecting STDIN from /dev/null.
    
    ## Checks
    
    - [x] All commits in this Pull Request are
    [signed](https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits)
    and Verified by Github.
    - [x] I have accepted the flox [Contributor License
    Agreement](../blob/main/.github/CLA.md) by adding my Git/Github details
    in a row at the end of the
    [CONTRIBUTORS.csv](../blob/main/.github/CONTRIBUTORS.csv) file by way of
    this pull request or one done previously.

--- lib/commands/development.sh
+++ lib/commands/development.sh
@@ -182,7 +182,7 @@ function flakeTopLevel() {
 	trace "$@"
 	local flakeRef=$1; shift
 	local url
-	if url=$($invoke_nix "${_nixArgs[@]}" flake metadata "$flakeRef" --json "$@" --override-input flox-floxpkgs/nixpkgs/nixpkgs flake:nixpkgs-$FLOX_STABILITY | $_jq -r .resolvedUrl); then
+	if url=$($invoke_nix "${_nixArgs[@]}" flake metadata "$flakeRef" --json "$@" --override-input flox-floxpkgs/nixpkgs/nixpkgs flake:nixpkgs-$FLOX_STABILITY 2>/dev/null </dev/null | $_jq -r .resolvedUrl); then
 		# strip git+file://
 		url="${url/git+file:\/\//}"
 		# strip path:
